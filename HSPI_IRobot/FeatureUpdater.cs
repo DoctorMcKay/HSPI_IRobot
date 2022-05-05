dc89e0c740623005fd734772d3fadbe100dbc22d
Alex Corn <alex@alexcorn.com>
2022-05-04 23:14:25 -0400

    Added status label for saving clean map edits

--- HSPI_IRobot/FeatureUpdater.cs
+++ HSPI_IRobot/FeatureUpdater.cs
@@ -12,11 +12,41 @@ namespace HSPI_IRobot {
 		
 		public void ExecuteFeatureUpdates(HsFeature feature) {
 			string[] addressParts = feature.Address.Split(':');
-			int featureVersion = int.Parse(feature.PlugExtraData["version"]);
+			int featureVersion = int.Parse(feature.PlugExtraData.ContainsNamed("version") ? feature.PlugExtraData["version"] : "1");
 
 			switch (addressParts[1]) {
+				case "Ready":
+					ExecuteReadyUpdate(feature, featureVersion);
+					break;
+				
 				case "Error":
 					ExecuteErrorUpdate(feature, featureVersion);
+					break;
+			}
+		}
+
+		private void ExecuteReadyUpdate(HsFeature feature, int featureVersion) {
+			switch (featureVersion) {
+				case 1:
+					_plugin.WriteLog(ELogType.Info, $"Updating feature {feature.Ref} (Ready) to version 2");
+					_plugin.GetHsController().DeleteStatusGraphicByValue(feature.Ref, 40);
+					_plugin.GetHsController().AddStatusGraphicToFeature(feature.Ref, new StatusGraphic(
+						"/images/HomeSeer/status/alarm.png",
+						40,
+						67
+					) { Label = "Not Ready" });
+					_plugin.GetHsController().AddStatusGraphicToFeature(feature.Ref, new StatusGraphic(
+						"/images/HomeSeer/status/alarm.png",
+						68,
+						"Saving smart map edits"
+					));
+					_plugin.GetHsController().AddStatusGraphicToFeature(feature.Ref, new StatusGraphic(
+						"/images/HomeSeer/status/alarm.png",
+						69,
+						255
+					) { Label = "Not Ready" });
+
+					UpdateFeatureVersionNumber(feature.Ref, 2);
 					break;
 			}
 		}
@@ -41,12 +71,21 @@ namespace HSPI_IRobot {
 						"Cannot connect to robot"
 					));
 
-					PlugExtraData extraData = feature.PlugExtraData;
-					extraData["version"] = "2";
-					_plugin.GetHsController().UpdatePropertyByRef(feature.Ref, EProperty.PlugExtraData, extraData);
-					
+					UpdateFeatureVersionNumber(feature.Ref, 2);
 					break;
 			}
+		}
+
+		private void UpdateFeatureVersionNumber(int featureRef, int version) {
+			PlugExtraData extraData = (PlugExtraData) _plugin.GetHsController().GetPropertyByRef(featureRef, EProperty.PlugExtraData);
+			
+			if (!extraData.ContainsNamed("version")) {
+				extraData.AddNamed("version", version.ToString());
+			} else {
+				extraData["version"] = version.ToString();
+			}
+
+			_plugin.GetHsController().UpdatePropertyByRef(featureRef, EProperty.PlugExtraData, extraData);
 		}
 	}
 }
