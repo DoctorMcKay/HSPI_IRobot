8c1daee4fd92da7532613c0ba1dd22483a9b7e52
Alex Corn <alex@alexcorn.com>
2022-05-28 02:44:34 -0400

    Prevent MQTTnet swallowing our exceptions

--- IRobotLANClient/MqttHandler.cs
+++ IRobotLANClient/MqttHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using MQTTnet;
 using MQTTnet.Client.Connecting;
 using MQTTnet.Client.Disconnecting;
@@ -6,25 +7,45 @@ using MQTTnet.Client.Receiving;
 
 namespace IRobotLANClient {
 	internal class MqttHandler : IMqttClientConnectedHandler, IMqttClientDisconnectedHandler, IMqttApplicationMessageReceivedHandler {
-		private readonly Robot Robot;
-		
+		private readonly Robot _robot;
+		private readonly TaskCompletionSource<Exception> _exceptionSource;
+
 		internal MqttHandler(Robot robot) {
-			Robot = robot;
+			_robot = robot;
+			_exceptionSource = new TaskCompletionSource<Exception>();
+
+			_exceptionSource.Task.ContinueWith(task => throw task.Result);
 		}
-		
+
 		public Task HandleConnectedAsync(MqttClientConnectedEventArgs eventArgs) {
-			Robot.ConnectedStateChanged(true);
+			// This is going to look ridiculous, but that's only because it is. MQTTnet swallows exceptions thrown in
+			// event handlers, because apparently continuing with undefined behavior if an exception isn't handled is
+			// preferable to crashing the app. So we have to escape this context to re-throw unhandled exceptions.
+			try {
+				_robot.ConnectedStateChanged(true);
+			} catch (Exception ex) {
+				_exceptionSource.SetResult(ex);
+			}
+			
 			return Task.CompletedTask;
 		}
 
 		public Task HandleDisconnectedAsync(MqttClientDisconnectedEventArgs eventArgs) {
-			Robot.ConnectedStateChanged(false);
+			try {
+				_robot.ConnectedStateChanged(false);
+			} catch (Exception ex) {
+				_exceptionSource.SetResult(ex);
+			}
+
 			return Task.CompletedTask;
 		}
 
-		public Task HandleApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs eventArgs) {
-			Robot.ApplicationMessageReceived(eventArgs.ApplicationMessage);
-			return Task.CompletedTask;
+		public async Task HandleApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs eventArgs) {
+			try {
+				await _robot.ApplicationMessageReceived(eventArgs.ApplicationMessage);
+			} catch (Exception ex) {
+				_exceptionSource.SetResult(ex);
+			}
 		}
 	}
 }
